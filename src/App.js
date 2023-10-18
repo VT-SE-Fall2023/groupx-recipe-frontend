e63888af3559f0eb87caf3df12c648dcf6c2956c
Yi-Han Chen <hannah51125@gmail.com>
2023-10-17 21:04:20 -0400

    feat: Create generate button in home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RecipeDisplay from './RecipeDisplay';
 
 function App() {
+  const [recipe, setRecipe] = useState(null);
+  const dummyRecipeData = {
+    recipeName: 'Pasta',
+    steps: [
+      'Place a large pot of lightly salted water (no more than 1 tablespoon salt) over high heat, and bring to a boil. Fill a large bowl with hot water for serving, and set aside.',
+      'In a mixing bowl, whisk together the eggs, yolks and pecorino and Parmesan. Season with a pinch of salt and generous black pepper.',
+    ],
+  };
+
+  const handleGenerateRecipe = () => {
+    // Replace this with your actual API endpoint
+    // fetch('YOUR_API_ENDPOINT_HERE')
+    //   .then((response) => response.json())
+    //   .then((data) => setRecipe(data))
+    //   .catch((error) => console.error('Error fetching data: ', error));
+    console.log("generate recipe button was clicked");
+    // Simulate an API call by setting the recipe data after a short delay
+    setTimeout(() => {
+      setRecipe(dummyRecipeData);
+    }, 500);
+  };
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +43,15 @@ function App() {
         >
           Learn React
         </a>
+        <button className="generate-button" onClick={handleGenerateRecipe}>
+          Generate
+        </button>
       </header>
+
+      <RecipeDisplay recipe={recipe} />
     </div>
   );
 }
 
 export default App;
+
