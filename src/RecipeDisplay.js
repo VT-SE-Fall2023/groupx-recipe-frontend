65a8e2890a0a6255277526630cd360b7a0673c04
Yi-Han Chen <hannah51125@gmail.com>
2023-10-20 23:11:36 -0400

    refactor: Move elements to RecipeDisplay component

--- src/RecipeDisplay.js
+++ src/RecipeDisplay.js
@@ -1,27 +1,79 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './css/RecipeDisplay.css'; // Import the CSS file
+import displayBg from './img/recipe-display-bg.jpg';
+import './App.css';
 
-function RecipeDisplay({ recipe }) {
-    if (!recipe) {
-        return null; // If recipe is not available, don't render anything
+function RecipeDisplay() {
+    const [recipe, setRecipe] = useState(null);
+    const dummyRecipeData = {
+        recipeName: 'Spaghetti Carbonara',
+        steps: [
+            'Place a large pot of lightly salted water (no more than 1 tablespoon salt) over high heat, and bring to a boil. Fill a large bowl with hot water for serving, and set aside.',
+            'In a mixing bowl, whisk together the eggs, yolks and pecorino and Parmesan. Season with a pinch of salt and generous black pepper.',
+            'Set the water to boil. Meanwhile, heat oil in a large skillet over medium heat, add the pork, and sauté until the fat just renders, on the edge of crispness but not hard. Remove from heat and set aside.',
+            'Add pasta to the water and boil until a bit firmer than al dente. Just before pasta is ready, reheat guanciale in skillet, if needed. Reserve 1 cup of pasta water, then drain pasta and add to the skillet over low heat. Stir for a minute or so.',
+            'Empty serving bowl of hot water. Dry it and add hot pasta mixture. Stir in cheese mixture, adding some reserved pasta water if needed for creaminess. Serve immediately, dressing it with a bit of additional grated pecorino and pepper.'
+        ],
+    };
+
+    let recipeDisplayRef = useRef(null);
+
+    function scrollTo(ref) {
+        if (!ref.current) return;
+        ref.current.scrollIntoView({ behavior: "smooth" });
     }
 
+    const handleGenerateRecipe = () => {
+        // Replace this with your actual API endpoint
+        // fetch('YOUR_API_ENDPOINT_HERE')
+        //   .then((response) => response.json())
+        //   .then((data) => setRecipe(data))
+        //   .catch((error) => console.error('Error fetching data: ', error));
+        console.log("generate recipe button was clicked");
+        // Simulate an API call by setting the recipe data after a short delay
+        setTimeout(() => {
+            setRecipe(dummyRecipeData);
+        }, 500);
 
-    const { recipeName, steps } = recipe;
+        // Scroll the recipe display into view
+        scrollTo(recipeDisplayRef);
+    };
+
+    const divStyle = {
+        backgroundImage: `url(${displayBg})`,
+        backgroundSize: 'cover', // Adjust as needed
+        backgroundRepeat: 'no-repeat', // Adjust as needed
+        backgroundPosition: 'center bottom', // Adjust as needed
+        height: '1000px',
+        display: 'flex',
+        padding: '5% 20%',
+    };
 
     return (
-        <div
-            className={`recipe-display ${recipe ? 'active' : ''}`}
-        >
-            <h2 className='recipe-name'>{recipeName}</h2>
-            {/* map over the steps array and render a list item for each step with the step header and step description */}
-            {steps.map((step, index) => (
-                <div key={`step+${index + 1}`}>
-                    <h3>Step {index + 1}</h3>
-                    <p>{step}</p>
-                </div>
-            ))}
+        <div className="container" >
+            <div className="ingridient-section" >
+                <button className="generate-button" onClick={handleGenerateRecipe}>
+                    Generate
+                </button>
+            </div>
 
+            <div style={divStyle} ref={recipeDisplayRef}>
+                {recipe ? (
+                    <div
+                        className={`recipe-display ${recipe ? 'active' : ''}`}
+                    >
+                        <h2 className='recipe-name'>{recipe.recipeName}</h2>
+                        {recipe.steps.map((step, index) => (
+                            <div key={`step+${index + 1}`}>
+                                <h3>Step {index + 1}</h3>
+                                <p>{step}</p>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p className="no-recipe-message">Click "Generate" to get a recipe.</p>
+                )}
+            </div>
         </div>
     );
 }
