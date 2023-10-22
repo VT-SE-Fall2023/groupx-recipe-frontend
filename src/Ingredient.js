56d412d4cbec09a955cf1c3b167ce8b89b999bb1
Chi Chen <stephchen55@gmail.com>
2023-10-21 22:13:21 -0400

    add button styling

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -5,10 +5,31 @@ function Ingredient() {
     return (
         <div className='ingredient-selection'>
             <section className='rectangle'></section>
-            <section className='buttons'>
-                <h4>Vegetables and Greens</h4>
-                <h4>Meats</h4>
-                <h4>Others</h4>
+            <section className='button-section'>
+                <h4 className="selection-title">Vegetables and Greens</h4>
+                <div className='selection-buttons'>
+                    <button>carrot</button>
+                    <button>potato</button>
+                    <button>tomato</button>
+                    <button>avacado</button>
+                    <button>spanich</button>
+                </div>
+                <h4 className="selection-title">Meats</h4>
+                <div className='selection-buttons'>
+                    <button>beef</button>
+                    <button>pork</button>
+                    <button>chicken</button>
+                    <button>salmon</button>
+                    <button>turkey</button>
+                </div>
+                <h4 className="selection-title">Others</h4>
+                <div className='selection-buttons'>
+                    <button>onion</button>
+                    <button>egg</button>
+                    <button>rice</button>
+                    <button>garlic</button>
+                    <button>cheese</button>
+                </div>
             </section>
         </div>
     );
