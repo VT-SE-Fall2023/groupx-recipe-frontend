d7a64acc41a14f9ba4891fc6647a3354c56240c2
Chi Chen <stephchen55@gmail.com>
2023-10-21 21:49:29 -0400

    add image source

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -4,7 +4,9 @@ import './css/Ingredient.css';
 function Ingredient() {
     return (
         <div className='ingredient-selection'>
-
+            <h4>Vegetables and Greens</h4>
+            <h4>Meats</h4>
+            <h4>Others</h4>
         </div>
     );
 }
