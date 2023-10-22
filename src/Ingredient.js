c2ab27ee737be6bfbd776c93bdcc8fb8f2312742
Chi Chen <stephchen55@gmail.com>
2023-10-21 21:58:23 -0400

    implement background in ingredient selection

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -4,9 +4,12 @@ import './css/Ingredient.css';
 function Ingredient() {
     return (
         <div className='ingredient-selection'>
-            <h4>Vegetables and Greens</h4>
-            <h4>Meats</h4>
-            <h4>Others</h4>
+            <section className='rectangle'></section>
+            <section className='buttons'>
+                <h4>Vegetables and Greens</h4>
+                <h4>Meats</h4>
+                <h4>Others</h4>
+            </section>
         </div>
     );
 }
