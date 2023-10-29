7c1bbcd33c94b2f224bd55587df93c4e35fcc0fb
Yi-Han Chen <hannah51125@gmail.com>
2023-10-29 18:26:24 -0400

    feat: Adjust Login.css

--- src/Login.js
+++ src/Login.js
@@ -18,32 +18,36 @@ function Login() {
     return (
         <div className="login-container">
             <div className="login-background">
-                <div className="login-form">
-                    <h2 className="welcome-text">Welcome back</h2>
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={handlePasswordChange}
-                    />
-                    <button className="login-button" onClick={handleLogin}>
-                        Login
-                    </button>
-                    <div className="login-options">
-                        <button className="login-google">
-                            Login with Google
-                            <img src={require('./img/google_icon.png')} alt="Google Logo" />
+                <div className="left-box">
+                </div>
+                <div className="right-box">
+                    <div className="login-form">
+                        <h2 className="welcome-text">Welcome Back</h2>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                        <button className="login-button" onClick={handleLogin}>
+                            Login
                         </button>
-                        <button className="login-apple">
-                            Login with Apple
-                            <img src={require('./img/apple_icon.png')} alt="Apple Logo" />
-                        </button>
+                        <div className="login-options">
+                            <button className="login-google">
+                                Login with Google
+                                <img src={require('./img/google_icon.png')} alt="Google Logo" />
+                            </button>
+                            <button className="login-apple">
+                                Login with Apple
+                                <img src={require('./img/apple_icon.png')} alt="Apple Logo" />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
