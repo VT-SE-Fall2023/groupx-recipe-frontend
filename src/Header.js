246c382ca1fbe1c0fa2522bd1f3cb095cf190022
Yi-Han Chen <hannah51125@gmail.com>
2023-10-30 21:54:48 -0400

    func: Set header with logc base on path

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import './css/Header.css'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import './css/Header.css';
 
 function Header() {
+    const { pathname } = useLocation();
     return (
         <div className='header'>
             <p id="logo">GroupX</p>
             <div className="about-login">
                 <p id='about'>About Us</p>
                 <button id='login-button'>
-                    <Link to="/login" id='login-button' className="login-button-header">
-                        Login
+                    <Link id='login-button' className="login-button-header"
+                        to={pathname === '/login' ? '/register' : '/login'}>
+                        {pathname === '/login' ? 'Register' : 'Login'}
                     </Link>
                 </button>
             </div>
