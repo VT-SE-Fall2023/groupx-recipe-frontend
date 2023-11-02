28e77d308b10889a8152a8411fe3df46a2e65df4
Chi Chen <stephchen55@gmail.com>
2023-11-02 13:46:56 -0400

    add title for History Page

---
+++ src/HistoryPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HistoryPage() {
+
+    return (
+        <div id="history-page">
+            <h1 className="main-title">Recipe History</h1>
+
+        </div>
+    );
+};
+
+export default HistoryPage;
