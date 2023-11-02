86c2659589e0bb0f0e2d4eb570b84eaf6c4ec6b6
Chi Chen <stephchen55@gmail.com>
2023-11-02 14:13:22 -0400

    add expand section

--- src/HistoryPage.js
+++ src/HistoryPage.js
@@ -5,12 +5,13 @@ import './css/HistoryPage.css';
 function HistoryPage() {
 
     return (
-        <div className="history-page">
-            <h1 className="main-title">Recipe History</h1>
-            <section className='expand-section'>
-                <h5>Spaghetti Carbonara</h5>
+        <div className='history-page'>
+            <h1 className='main-title'>Recipe History</h1>
+            <section className='menus'>
+                <div className='expand-section'>
+                    <h5 className='menu-title'>Spaghetti Carbonara</h5>
+                </div>
             </section>
-
         </div>
     );
 };
