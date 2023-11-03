f5ec5facb3e78a6d3ecde4b4cda64c658e4e9f11
Yi-Han Chen <hannah51125@gmail.com>
2023-11-03 10:17:13 -0400

    func: Add register success page

---
+++ src/RegistrationSuccess.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function RegistrationSuccess() {
+    return (
+        <div>
+            <h2>Registration Successful</h2>
+            <p>Your registration was successful. You can now log in.</p>
+            {/* You can add additional content as needed. */}
+        </div>
+    );
+}
+
+export default RegistrationSuccess;
