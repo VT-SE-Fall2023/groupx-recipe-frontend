8373731de3840dd158425436679654e9aa624f9f
Chi Chen <stephchen55@gmail.com>
2023-11-14 00:30:53 -0500

    handle ingredient selection

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -9,6 +9,19 @@ function Ingredient() {
     const meats = ["beef", "pork", "chicken", "salmon", "turkey"];
     const others = ["onion", "egg", "rice", "garlic", "cheese"];
 
+    //function to check whether the ingredient is selected
+    const findItem = (selectedItems, ingredient) => selectedItems.find((item)=>item === ingredient)
+
+    const handleButtonClick = (ingredient) => {
+        // Check if the item is already selected
+        if (!selectedItems.includes(ingredient)) {
+          setSelectedItems([...selectedItems, ingredient]);
+        } else { //remove the item from the list if it's already selected.
+            setSelectedItems(selectedItems.filter((item)=>item!==ingredient));
+        }
+        console.log(selectedItems)
+      };
+
     return (
         <div className='ingredient-selection'>
             <section className='rectangle'></section>
@@ -16,19 +29,31 @@ function Ingredient() {
                 <h4 className="selection-title">Vegetables and Greens</h4>
                 <div className='selection-buttons'>
                     {veggies.map((item)=>(
-                        <button className='menu-button'>{item}</button>
+                        <button 
+                            className={findItem(selectedItems, item) ? 'selected-button' : 'menu-button'}
+                            onClick={()=>handleButtonClick(item)}>
+                            {item}
+                        </button>
                     ))}
                 </div>
                 <h4 className="selection-title">Meats</h4>
                 <div className='selection-buttons'>
                     {meats.map((item)=>(
-                        <button className='menu-button'>{item}</button>
+                        <button 
+                            className='menu-button'
+                            onClick={()=>handleButtonClick(item)}>
+                            {item}
+                        </button>
                     ))}
                 </div>
                 <h4 className="selection-title">Others</h4>
                 <div className='selection-buttons'>
                     {others.map((item)=>(
-                        <button className='menu-button'>{item}</button>
+                        <button 
+                        className='menu-button'
+                        onClick={()=>handleButtonClick(item)}>
+                            {item}
+                        </button>
                     ))}
                 </div>
             </section>
