f8cdadeb15d8e31226d8de965700f533fe073401
Yi-Han Chen <hannah51125@gmail.com>
2023-11-13 19:45:56 -0500

    test: Test input update state

--- src/Register.test.js
+++ src/Register.test.js
@@ -1,7 +1,23 @@
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
 import Register from './Register';
 
 test('renders Register component', () => {
     render(<Register />);
 });
+
+test('input fields update state', () => {
+    const { getByPlaceholderText } = render(<Register />);
+    const emailInput = getByPlaceholderText('Email');
+    const passwordInput = getByPlaceholderText('Password');
+    const confirmPasswordInput = getByPlaceholderText('Confirm Password');
+
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'test123' } });
+    fireEvent.change(confirmPasswordInput, { target: { value: 'test123' } });
+
+    expect(emailInput.value).toBe('test@example.com');
+    expect(passwordInput.value).toBe('test123');
+    expect(confirmPasswordInput.value).toBe('test123');
+});
+
