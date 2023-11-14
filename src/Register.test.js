2cc71fc8462cd4681455f48a021a1656888486ef
Yi-Han Chen <hannah51125@gmail.com>
2023-11-13 19:23:30 -0500

    test: Create first test for Register Component

---
+++ src/Register.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Register from './Register';
+
+test('renders Register component', () => {
+    render(<Register />);
+});
