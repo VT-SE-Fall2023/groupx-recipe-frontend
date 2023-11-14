81700045ebe48e859e6f7e18e55cb72185c9f492
Chi Chen <stephchen55@gmail.com>
2023-11-13 23:36:37 -0500

    use map to build buttons

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,34 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 import './css/Ingredient.css';
 
 function Ingredient() {
+    //useState for storing selected items
+    const [selectedItems, setSelectedItems] = useState([]);
+    const veggies = ["carrot", "potato", "tomato", "avacado", "spanich"];
+    const meats = ["beef", "pork", "chicken", "salmon", "turkey"];
+    const others = ["onion", "egg", "rice", "garlic", "cheese"];
+
     return (
         <div className='ingredient-selection'>
             <section className='rectangle'></section>
             <section className='button-section'>
                 <h4 className="selection-title">Vegetables and Greens</h4>
                 <div className='selection-buttons'>
-                    <button className='menu-button'>carrot</button>
-                    <button className='menu-button'>potato</button>
-                    <button className='menu-button'>tomato</button>
-                    <button className='menu-button'>avacado</button>
-                    <button className='menu-button'>spanich</button>
+                    {veggies.map((item)=>(
+                        <button className='menu-button'>{item}</button>
+                    ))}
                 </div>
                 <h4 className="selection-title">Meats</h4>
                 <div className='selection-buttons'>
-                    <button className='menu-button'>beef</button>
-                    <button className='default-button'>pork</button>
-                    <button className='menu-button'>chicken</button>
-                    <button className='menu-button'>salmon</button>
-                    <button className='menu-button'>turkey</button>
+                    {meats.map((item)=>(
+                        <button className='menu-button'>{item}</button>
+                    ))}
                 </div>
                 <h4 className="selection-title">Others</h4>
                 <div className='selection-buttons'>
-                    <button className='menu-button'>onion</button>
-                    <button className='default-button'>egg</button>
-                    <button className='menu-button'>rice</button>
-                    <button className='menu-button'>garlic</button>
-                    <button className='default-button'>cheese</button>
+                    {others.map((item)=>(
+                        <button className='menu-button'>{item}</button>
+                    ))}
                 </div>
             </section>
         </div>
