999beaa8f0b51d4f260c7598f4993efe0bb168bb
Chi Chen <stephchen55@gmail.com>
2023-11-14 17:31:49 -0500

    delete ratingElement (not needed)

--- src/HistoryElement.js
+++ src/HistoryElement.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import './css/HistoryElement.css';
-import Rating from './RatingElement';
 
 //props include: name, date, rate, ingredients, steps
 function HistoryElement(props) {
