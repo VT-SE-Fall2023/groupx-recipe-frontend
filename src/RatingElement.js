edd1cdff0965d2f9bc5cd26c1669e4662614930f
Chi Chen <stephchen55@gmail.com>
2023-11-14 17:17:54 -0500

    add rating element

---
+++ src/RatingElement.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Rating = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleStarClick = (clickedRating) => {
+    setRating(clickedRating);
+  };
+
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      const starImg = i <= rating
+        ? <img key={i} src={require('./img/fullstar-icon.png')} alt="Full Star Logo" onClick={() => handleStarClick(i)} />
+        : <img key={i} src={require('./img/emptystar-icon.png')} alt="Empty Star Logo" onClick={() => handleStarClick(i)} />;
+      stars.push(starImg);
+    }
+    return stars;
+  };
+
+  return (
+    <div className="rating">
+      {renderStars()}
+    </div>
+  );
+};
+
+export default Rating;
