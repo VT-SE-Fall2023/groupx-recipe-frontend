a60894890e97945eb4228bca118521deafd6d4a9
Yi-Han Chen <hannah51125@gmail.com>
2023-11-27 19:45:42 -0500

    feat: Remove history button in header when logout

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import './css/Header.css';
-import { useHistoryContext } from './context/HistoryContext';
 import { useEmailContext } from './context/EmailContext';
+import { useHistoryContext } from './context/HistoryContext';
+import './css/Header.css';
 
 function Header() {
     const { pathname } = useLocation();
     const navigate = useNavigate();
     const isAuthenticated = !!localStorage.getItem('email'); // Check if the user is authenticated
-    const {setUserHistory} = useHistoryContext();
-    const {handleLoginEmail} = useEmailContext();
+    const { setUserHistory } = useHistoryContext();
+    const { handleLoginEmail } = useEmailContext();
 
     const handleLogout = () => {
         // Remove the token from localStorage or perform any other necessary cleanup
@@ -24,9 +24,11 @@ function Header() {
         <div className='header'>
             <Link id="logo" to='/'>GroupX</Link>
             <div className="about-login">
-                <Link id='about' to='/history'>
-                    History
-                </Link>
+                {isAuthenticated && ( // Show the "History" link only when the user is authenticated
+                    <Link id='about' to='/history'>
+                        History
+                    </Link>
+                )}
                 {isAuthenticated ? ( // Render the "Logout" button if the user is authenticated
                     <button id='login-button' onClick={handleLogout}>
                         Logout
